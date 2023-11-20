43e0d29008d632d7a368e8736d4ca80b2215fffa
geoff <geoff@generalconsulting.io>
2023-11-19 20:47:19 -0500

    capturing new nixos config

--- deploy_home.sh
+++ deploy_home.sh
@@ -1,1 +1,3 @@
-cp home.nix ~/.config/home-manager/home.nix && home-manager switch
+home-manager switch -n -I ./home.nix &&  git add . && git commit -m 'capturing new nixos config' \
+&& home-manager switch -I config=./configuration.nix 
+git push
