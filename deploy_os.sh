bbf50a7bcd22a5d426966312c401725912fcf0f7
geoff <geoff@generalconsulting.io>
2023-11-19 20:40:04 -0500

    capturing new nixos config

--- deploy_os.sh
+++ deploy_os.sh
@@ -1,2 +1,3 @@
-git add . && git commit -m 'capturing new nixos config'
-nixos-rebuild dry-build && sudo nixos-rebuild switch -I nixos-config=./configuration.nix
+nixos-rebuild dry-build && git add . && git commit -m 'capturing new nixos config'
+sudo nixos-rebuild switch -I nixos-config=./configuration.nix
+git push
