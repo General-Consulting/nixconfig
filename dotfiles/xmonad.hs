6726fa6da4a36561d7f81d5e4d59307a0f84bcf5
geoff <geoff@generalconsulting.io>
2023-11-22 06:00:36 -0500

    capturing new home-manager config

--- dotfiles/xmonad.hs
+++ dotfiles/xmonad.hs
@@ -22,7 +22,7 @@ myConfig = def
     }
   `additionalKeysP`
     [ ("M-S-z", spawn "xscreensaver-command -lock")
-    , ("M-C-s", unGrab *> spawn "scrot -s 'xclip -selection clipboard -t image/png -i $f'"        )
+    , ("M-C-s", unGrab *> spawn "scrot -s -o /dev/stdout | xclip -selection clipboard -t image/png -i $f"        )
     , ("M-g"  , spawn "google-chrome-stable --user-data-directory=/home/geoff/.config/google-chrome/Profile\\ 1/"                   )
     , ("M-f"  , spawn "firefox"                   )
     , ("M-p"  , spawn "rofi -show run")
