b4f5c89b17493019e89204dfb698e2bf0c6b611f
geoff <geoff@generalconsulting.io>
2023-12-02 08:00:52 -0500

    capturing new home-manager config

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -191,7 +191,7 @@ myManageHook = composeAll
   , transience'
   ]
 
-myLayouts = spacing 10 $ tiled ||| Mirror tiled ||| Full 
+myLayouts = spacing 18 $ tiled ||| Mirror tiled ||| Full 
   where
     tiled    = Tall nmaster delta ratio
     nmaster  = 1      -- Default number of windows in the master pane
