51ec2633dac05d7642624642c6d99fa9266e2534
geoff <geoff@generalconsulting.io>
2023-12-02 05:01:22 -0500

    testing lazygit
    it's fucking awesome

--- home.nix
+++ home.nix
@@ -34,8 +34,6 @@
     lazygit
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
    ".Xmodmap".source = ./dotfiles/Xmodmap;
    ".xmonad/" = {
