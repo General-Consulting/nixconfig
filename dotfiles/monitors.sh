ad65490e4f091b725ae928724ceaac903ea1ab30
geoff <geoff@generalconsulting.io>
2023-12-04 20:34:34 -0500

    capturing new home-manager config

--- dotfiles/monitors.sh
+++ dotfiles/monitors.sh
@@ -3,7 +3,7 @@ myDP=DP-3
 xrandr --output $myHDMI --auto --primary
 # If we find that a screen is connected, activate it and position it
 # to the right of the primary screen.
-xrandr | grep $myDP\ connected | ifne xrandr --output $myDP --auto --right-of $myDisplay
+xrandr | grep $myDP\ connected | ifne xrandr --output $myDP --auto --left-of $myHDMI
 
 #debugging
 xrandr >xrandr.output.log
