9ac75df5f9ac6b57dfa742f37e5d45e91f384c94
geoff <geoff@generalconsulting.io>
2023-12-07 04:53:43 -0500

    capturing new home-manager config

--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -1,4 +1,23 @@
 return {
+
+
+{
+  "williamboman/mason.nvim",
+  opts = function(_, opts)
+    opts.ensure_installed = opts.ensure_installed or {}
+    table.insert(opts.ensure_installed, "js-debug-adapter")
+  end,
+},
+
+  {
+  "nvim-treesitter/nvim-treesitter",
+  opts = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      vim.list_extend(opts.ensure_installed, { "typescript", "tsx" })
+    end
+  end,
+},
+
 {
   "mfussenegger/nvim-dap",
   optional = true,
@@ -58,5 +77,65 @@ return {
       end
     end
   end,
+},
+
+{
+  "neovim/nvim-lspconfig",
+  opts = {
+    -- make sure mason installs the server
+    servers = {
+      ---@type lspconfig.options.tsserver
+      tsserver = {
+        keys = {
+          {
+            "<leader>co",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.organizeImports.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Organize Imports",
+          },
+          {
+            "<leader>cR",
+            function()
+              vim.lsp.buf.code_action({
+                apply = true,
+                context = {
+                  only = { "source.removeUnused.ts" },
+                  diagnostics = {},
+                },
+              })
+            end,
+            desc = "Remove Unused Imports",
+          },
+        },
+        settings = {
+          typescript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          javascript = {
+            format = {
+              indentSize = vim.o.shiftwidth,
+              convertTabsToSpaces = vim.o.expandtab,
+              tabSize = vim.o.tabstop,
+            },
+          },
+          completions = {
+            completeFunctionCalls = true,
+          },
+        },
+      },
+    },
+  },
 }
+
 }
