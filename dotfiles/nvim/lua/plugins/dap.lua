82bc4c7b97ce1a58d0011cda6cbd9157418caf8e
geoff <geoff@generalconsulting.io>
2024-01-08 20:21:31 -0500

    capturing new home-manager config

--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -18,6 +18,18 @@ return {
   end,
 },
 
+ {
+   "nvimtools/none-ls.nvim",
+   opts = function(_, opts)
+     local nls = require("null-ls")
+     opts.sources = vim.list_extend(opts.sources, {
+       -- Order of formatters matters. They are used in order of appearance.
+       nls.builtins.formatting.ruff,
+       nls.builtins.formatting.black,
+       nls.builtins.diagnostics.ruff,
+     })
+   end,
+ },
 {
   "mfussenegger/nvim-dap",
   optional = true,
