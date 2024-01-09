035ab58410977330caff06c0cf26d09c2b78a15c
geoff <geoff@generalconsulting.io>
2024-01-08 22:27:01 -0500

    capturing new home-manager config

--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -147,7 +147,6 @@ return {
           require("lazyvim.util").lsp.on_attach(function(client, _)
             if client.name == "pyright" then
               -- disable hover in favor of jedi-language-server
-              client.server_capabilities.hoverProvider = false
             end
           end)
         end,
