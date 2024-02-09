e7b618115b43d54d967694ea5cf28ab639dd1d03
geoff <geoff@generalconsulting.io>
2024-02-08 19:36:06 -0500

    remove unused lsp stuff

--- dotfiles/nvim/lua/config/lazy.lua
+++ dotfiles/nvim/lua/config/lazy.lua
@@ -14,11 +14,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.dap.core" },
-    {
-      "pmizio/typescript-tools.nvim",
-      dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-      opts = {},
-    },
     -- { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -17,20 +17,6 @@ return {
     end
   end,
 },
-
- {
-   "nvimtools/none-ls.nvim",
-   opts = function(_, opts)
-     local nls = require("null-ls")
-     opts.sources = vim.list_extend(opts.sources, {
-       -- Order of formatters matters. They are used in order of appearance.
-       nls.builtins.formatting.ruff_format,
-       nls.builtins.formatting.ruff,
-       nls.builtins.code_actions.refactoring,
-       nls.builtins.code_actions.statix,
-     })
-   end,
- },
 {
   "mfussenegger/nvim-dap",
   optional = true,
