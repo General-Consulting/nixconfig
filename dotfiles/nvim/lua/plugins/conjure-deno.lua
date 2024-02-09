f7656831855e9eae6f5bf4441e3eb1886bd28217
geoff <geoff@generalconsulting.io>
2024-02-08 23:21:33 -0500

    fix syntax

--- dotfiles/nvim/lua/plugins/conjure-deno.lua
+++ dotfiles/nvim/lua/plugins/conjure-deno.lua
@@ -3,7 +3,7 @@ return {
   depenpendcies = {{
     'nvim-treesitter/nvim-treesitter',
     config = function()
-      require('nvim-treesitter.install').update({ with_sync = true }
+      require('nvim-treesitter.install').update({ with_sync = true })
     end,
   },
     {
