3fdf466bf3eed215fbb647c3fc60b8094b66bec8
geoff <geoff@generalconsulting.io>
2024-02-08 23:20:24 -0500

    deno lsp

---
+++ dotfiles/nvim/lua/plugins/conjure-deno.lua
@@ -0,0 +1,12 @@
+return {
+  'sigmaSd/conjure-deno',
+  depenpendcies = {{
+    'nvim-treesitter/nvim-treesitter',
+    config = function()
+      require('nvim-treesitter.install').update({ with_sync = true }
+    end,
+  },
+    {
+      'Olical/conjure'
+    }}
+}
