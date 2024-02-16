9a6379442b2bf164340fed76365c6479b8b16dd0
geoff <geoff@generalconsulting.io>
2024-02-16 12:29:04 -0500

    patched obsidian

--- home.nix
+++ home.nix
@@ -1,4 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let obsidian = lib.throwIf (lib.versionOlder "1.5.3" pkgs.obsidian.version) "Obsidian no longer requires EOL Electron" (
+    pkgs.obsidian.override {
+      electron = pkgs.electron_25.overrideAttrs (_: {
+        preFixup = "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+        meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
+      });
+    }
+  );
+in
 {
 
   home.username = "geoff";
@@ -45,6 +54,7 @@
     ffmpeg
     gnome.nautilus
     cargo
+    obsidian
   ];
 
   home.file = {
