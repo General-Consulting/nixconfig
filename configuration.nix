4993fe8180d4d36291999209a080febb265076a1
geoff <geoff@generalconsulting.io>
2024-02-18 04:45:28 -0500

    capturing new nixos config

--- configuration.nix
+++ configuration.nix
@@ -225,7 +225,17 @@
 
   system.stateVersion = "24.05"; 
 
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+    };
+    };
   
   networking.extraHosts = 
 	'' 
