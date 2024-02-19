e574197b6dacda519979fec59371d63884d8d8ab
geoff <geoff@generalconsulting.io>
2024-02-19 14:13:33 -0500

    last one

--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -22,6 +22,21 @@ return {
   } 
 
   opts = function()
+    if not dap.adapters["node-terminal"] then
+      require("dap").adapters["pwa-node"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          args = {
+            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+              .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+    end
     for _, language in ipairs({ "typescript", "javascript" }) do
       require("dap").adapters[language] = {
               {
@@ -58,23 +73,6 @@ return {
       },
   },
   opts = function()
-    local dap = require("dap")
-    if not dap.adapters["node-terminal"] then
-      require("dap").adapters["pwa-node"] = {
-        type = "server",
-        host = "localhost",
-        port = "${port}",
-        executable = {
-          command = "node",
-          -- 💀 Make sure to update this path to point to your installation
-          args = {
-            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
-              .. "/js-debug/src/dapDebugServer.js",
-            "${port}",
-          },
-        },
-      }
-    end
     if not dap.adapters["pwa-node"] then
       require("dap").adapters["pwa-node"] = {
         type = "server",
