57b03847ed1e310d15d416cab6a47ad44aaaa067
geoff <geoff@generalconsulting.io>
2024-02-19 13:55:40 -0500

    add node-terminal

--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -41,6 +41,22 @@ return {
   },
   opts = function()
     local dap = require("dap")
+    if not dap.adapters["node-terminal"] then
+      require("dap").adapters["node-terminal"] = {
+        type = "server",
+        host = "localhost",
+        port = "${port}",
+        executable = {
+          command = "node",
+          -- 💀 Make sure to update this path to point to your installation
+          args = {
+            require("mason-registry").get_package("js-debug-adapter"):get_install_path()
+              .. "/js-debug/src/dapDebugServer.js",
+            "${port}",
+          },
+        },
+      }
+    end
     if not dap.adapters["pwa-node"] then
       require("dap").adapters["pwa-node"] = {
         type = "server",
