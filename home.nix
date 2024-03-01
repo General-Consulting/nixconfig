f0777e06e438bd4a2a0feaa8243e1747677b7706
geoff <geoff@generalconsulting.io>
2024-03-01 11:42:46 -0500

    add flameshot

--- home.nix
+++ home.nix
@@ -1,14 +1,15 @@
 { pkgs, lib, ... }:
-let
-  obsidian = lib.throwIf (lib.versionOlder "1.5.3" pkgs.obsidian.version)
-    "Obsidian no longer requires EOL Electron" (pkgs.obsidian.override {
-      electron = pkgs.electron_25.overrideAttrs (_: {
-        preFixup =
-          "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
-        meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
-      });
-    });
-in {
+#let
+#  obsidian = lib.throwIf (lib.versionOlder "1.5.3" pkgs.obsidian.version)
+#    "Obsidian no longer requires EOL Electron" (pkgs.obsidian.override {
+#      electron = pkgs.electron_25.overrideAttrs (_: {
+#        preFixup =
+#          "patchelf --add-needed ${pkgs.libglvnd}/lib/libEGL.so.1 $out/bin/electron"; # NixOS/nixpkgs#272912
+#        meta.knownVulnerabilities = [ ]; # NixOS/nixpkgs#273611
+#      });
+#    });
+#in 
+{
   home.username = "geoff";
   home.homeDirectory = "/home/geoff";
 
@@ -24,6 +25,7 @@ in {
     synergy
     direnv
     google-chrome
+    flameshot
     xclip
     scrot
     newman
