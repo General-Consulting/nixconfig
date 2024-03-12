870b09aed677d485d536017236282692a96de71d
geoff <geoff@generalconsulting.io>
2024-03-12 15:01:29 -0400

    flameshot only in startup.sh

--- dotfiles/.xmonad/xmonad.hs
+++ dotfiles/.xmonad/xmonad.hs
@@ -171,7 +171,6 @@ myStartupHook = do
 --  spawnOnce "alacritty -T currentProj --class currentProj,alacritty -e sh -c \"cd src/pdf && nix develop && nvim .\""
   spawnOnce "obsidian"
   spawnOnce "google-chrome-stable"
-  spawnOnce "flameshot"
 --  spawnOnce
 --    "trayer --edge top --align right --SetDockType true --SetPartialStrut true --expand true --width 5 --height 18 --transparent true --tint 0x5f5f5f &"
   -- workaround for Java Swing/GUI apps not working
