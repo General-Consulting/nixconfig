42b05d91e2e96a7bed4c008feea4f6d0f856ae35
geoff <geoff@generalconsulting.io>
2024-04-12 19:42:30 -0400

    Capturing new home-manager config

--- dotfiles/nvim/lua/plugins/chatgpt.lua
+++ dotfiles/nvim/lua/plugins/chatgpt.lua
@@ -10,19 +10,4 @@
       "folke/trouble.nvim",
       "nvim-telescope/telescope.nvim"
     }
-  keys = {
-    {"<leader>cc" , "<cmd>ChatGPT<CR>", desc = "ChatGPT" },
-    {"<Leader>ce" , "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
-    {"<Leader>cg" , "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
-    {"<Leader>ct" , "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
-    {"<Leader>ck" , "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
-    {"<Leader>cd" , "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
-    {"<Leader>ca" , "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
-    {"<Leader>co" , "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
-    {"<Leader>cs" , "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
-    {"<Leader>cf" , "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
-    {"<Leader>cx" , "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
-    {"<Leader>cr" , "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
-    {"<Leader>cl" , "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
-}
   }
