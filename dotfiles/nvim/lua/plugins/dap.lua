8d8c89137ea0822c7e28af12dfe27656c025c699
geoff <geoff@generalconsulting.io>
2024-04-12 22:13:44 -0400

    Capturing new home-manager config

--- dotfiles/nvim/lua/plugins/dap.lua
+++ dotfiles/nvim/lua/plugins/dap.lua
@@ -128,7 +128,7 @@ return {
             -- Path to vscode-js-debug installation.
             -- Command to use to lkeaunch the debug server. Takes precedence over "node_path" and "debugger_path"
             -- debugger_cmd = { "js-debug-adapter" },
-            debugger_path = vim.fn.resolve(vim.fn.stdpath("data") .. "/lazy/vscode-js-debug"),
+            debugger_path = vim.fn.resolve("/home/geoff/.DAP/vscode-js-debug/"),
 
             -- which adapters to register in nvim-dap
             adapters = {
