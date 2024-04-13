13f431e2b52489c58b8e98e9de24cecd51bf4f62
geoff <geoff@generalconsulting.io>
2024-04-12 20:10:10 -0400

    Capturing new home-manager config

--- dotfiles/nvim/lua/plugins/which-key.lua
+++ dotfiles/nvim/lua/plugins/which-key.lua
@@ -2,7 +2,7 @@ return {
   "folke/which-key.nvim",
   opts = {
     defaults = {
-      z = {
+      c = {
         name = "ChatGPT",
         c = { "<cmd>ChatGPT<CR>", "ChatGPT" },
         e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
